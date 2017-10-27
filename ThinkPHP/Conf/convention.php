f50b5d9e305b5714e4db9c7c597231324e0b6c91
Jago <yejian122@qq.com>
2017-10-27 08:51:53 +0800

    jago modified config convention

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -54,7 +54,7 @@ return  array(
 
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'www.jago.com', // 服务器地址
+    'DB_HOST'               =>  'www.ddkj.com', // 服务器地址
     'DB_NAME'               =>  'usedcar',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
